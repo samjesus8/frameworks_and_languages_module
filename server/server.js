af43a4d12a5d4dcd19b3780d27ce0da49e9735f9
Samuel Jesuthas <98812930+samjesus8@users.noreply.github.com>
2023-10-30 11:16:40 +0000

    [ASSIGNMENT] changed itemId into id

--- server/server.js
+++ server/server.js
@@ -7,7 +7,21 @@ app.use(express.json());
 app.use(cors())
 
 let ITEMS = [
-
+  {
+    "id": 3528640174782866,
+    "user_id": "Sam1234",
+    "keywords": [
+      "Word1",
+      " Word2",
+      " Word3"
+    ],
+    "description": "111111111111",
+    "image": "https://i.imgur.com/SCEwQdK.jpeg",
+    "lat": "13.16937",
+    "lon": "-82.39787",
+    "date_from": "2023-10-30T11:09:14.606Z",
+    "date_to": "2023-10-30T11:09:14.606Z"
+  }
 ]
 
 app.get('/', (req, res) => {
@@ -15,46 +29,46 @@ app.get('/', (req, res) => {
 })
 
 app.get('/items', (req, res) => {
-  console.log("GET 200")
+  console.log("GET 200");
   res.json(ITEMS)
 })
 
 app.get('/item/:itemId', (req, res) => {
-  const itemId = parseFloat(req.params.itemId); // Parse the ID parameter
+  const itemId = parseFloat(req.params.id); // Parse the ID parameter
 
   // Find the item with the specified ID
-  const item = ITEMS.find(item => item.itemId === itemId);
+  const item = ITEMS.find(item => item.id === itemId);
 
   // Check if the item was found
   if (!item) {
-    console.log("GET /item/{itemId} 404 itemId: " + itemId);
+    console.log("GET /item/{itemId} 404 id: " + itemId);
     return res.status(404).json({ message: 'Item not found' });
   }
 
   // Respond with a 200 status code and the specific item in valid JSON format
-  console.log("GET /item/{itemId} 200")
+  console.log("GET /item/{itemId} 200");
   res.status(200).json(item);
 })
 
 app.post('/item', (req, res) => {
-  if (Object.keys(req.body).sort().toString() != "date_from,date_to,description,image,itemId,keywords,lat,lon,user_id"){
+  if (Object.keys(req.body).sort().toString() != "date_from,date_to,description,id,image,keywords,lat,lon,user_id"){
     console.log("POST 405");
     console.log(req.body);
     return res.status(405).json({"message": "missing fields"})
   }
   ITEMS.push(req.body)
-  console.log("POST 201")
+  console.log("POST 201");
   res.status(201).json()
 })
 
 app.delete('/item/:itemId', (req, res) => {
-  const itemId = parseFloat(req.params.itemId); // Parse the ID parameter
+  const itemID = parseFloat(req.params.id); // Parse the ID parameter
 
   // Find the index of the item with the specified ID
-  const itemIndex = ITEMS.findIndex(item => item.itemId === itemId);
+  const itemIndex = ITEMS.findIndex(item => item.id === itemID);
 
   // Check if the item was found
-  if (itemIndex === -1) {
+  if (itemIndex === 0) {
     return res.status(404).json({ message: 'Item not found' });
   }
 
@@ -62,12 +76,12 @@ app.delete('/item/:itemId', (req, res) => {
   ITEMS.splice(itemIndex, 1);
 
   // Respond with a 204 status code indicating a successful deletion
-  console.log("DELETE 204 itemId: " + itemId);
+  console.log("DELETE 204 id: " + itemID);
   res.status(204).json();
 })
 
 app.listen(port, () => {
-  console.log(`Samuel Jesuthas Assignment listening on port ${port}`)
+  console.log(`Samuel Jesuthas Assignment listening on port ${port}`);
 })
 
 process.on('SIGINT', function() {process.exit()})
