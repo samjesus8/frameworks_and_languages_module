6700f3022e072e70fa71cca8f55bdfbba05464e1
Samuel Jesuthas <98812930+samjesus8@users.noreply.github.com>
2023-12-11 10:58:13 +0000

    [ASSIGNMENT] Individual Server tests passed

--- server/independent_server/server.py
+++ server/independent_server/server.py
@@ -2,11 +2,29 @@ from wsgiref.simple_server import make_server
 import falcon
 from falcon_cors import CORS
 import json
-from datetime import datetime
+import datetime
 
 # "pip install falcon-cors" in order to use this
 # Enabling use of CORS middleware (OPTIONS request)
 cors = CORS(allow_all_origins=True, allow_all_headers=True, allow_all_methods=True)
+
+items = [  
+    {
+        "id": 3528640174782866,
+        "user_id": "Sam1234",
+        "keywords": [
+            "Word1",
+            "Word2",
+            "Word3"
+        ],
+        "description": "111111111111",
+        "image": "https://i.imgur.com/SCEwQdK.jpeg",
+        "lat": 13.16937,
+        "lon": -82.39787,
+        "date_from": "2023-10-30T11:09:14.606Z",
+        "date_to": "2023-10-30T11:09:14.606Z"
+    }
+]
 
 class RootResource:
     def on_get(self, req, resp):
@@ -27,147 +45,85 @@ class RootResource:
     def on_options(self, req, resp):
         # Respond to CORS requests
         resp.status = falcon.HTTP_204
-        resp.set_header('Allow', 'GET, OPTIONS')
-        resp.set_header('Access-Control-Allow-Origin', '*')
-        resp.set_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
-        resp.set_header('Access-Control-Allow-Headers', '*')
-
-class ItemsResource:
-    items = [  
-        {
-            "id": 3528640174782866,
-            "user_id": "Sam1234",
-            "keywords": [
-                "Word1",
-                "Word2",
-                "Word3"
-            ],
-            "description": "111111111111",
-            "image": "https://i.imgur.com/SCEwQdK.jpeg",
-            "lat": 13.16937,
-            "lon": -82.39787,
-            "date_from": "2023-10-30T11:09:14.606Z",
-            "date_to": "2023-10-30T11:09:14.606Z"
-        }
-    ]
-
-    def on_get(self, req, resp): ## Handles GET /items
-        resp.status = falcon.HTTP_200
-        resp.text = json.dumps(self.items, indent=2)
-
-    def get_item_by_id(self, item_id): ## Handles GET /item/{itemId}
-        for item in ItemsResource.items:
-            if item["id"] == item_id:
-                return item
-        return None
-
-    def get_item_index_by_id(self, item_id):
-        for i, item in enumerate(self.items):
-            if item["id"] == item_id:
-                return i
-        return -1
-
-class ItemResource:
-    def on_get(self, req, resp, item_id):
-        # Parse the ID parameter
-        item_id = int(item_id)
-
-        # Find the item with the specified ID
-        item = items.get_item_by_id(item_id)
-        if item:
-            resp.status = falcon.HTTP_200
-            resp.text = json.dumps(item, indent=2)
-        else:
-            resp.status = falcon.HTTP_404
-            resp.text = "Item not found"
-
-    def get_item_by_id(self, item_id): ## Handles GET /item/{itemId}
-        for item in ItemsResource.items:
-            if item["id"] == item_id:
-                return item
-        return None
+        resp.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
 
 class CreateItemResource:
     def on_post(self, req, resp):
         # Parse the JSON payload from the request body
-        try:
-            req_data = json.loads(req.bounded_stream.read().decode('utf-8'))
-            print(req_data)
-        except json.JSONDecodeError:
-            resp.status = falcon.HTTP_400
-            resp.text = "Invalid JSON payload"
-            return
+        req_data = json.loads(req.bounded_stream.read().decode('utf-8'))
 
-        # Check if all expected fields are present in the request body and have valid non-empty values
-        expected_fields = ['user_id', 'keywords', 'description', 'image', 'lat', 'lon']
-
-        # If 'image' is provided, check for 6 fields; otherwise, stick to 5
-        if 'image' in req_data:
-            print(req_data)
-        else:
-            expected_fields.remove('image')
-
-        if not all(field in req_data and req_data[field] for field in expected_fields):
+        if req_data.get("user_id") is None or req_data.get("keywords") is None or req_data.get("description") is None or req_data.get("lat") is None or req_data.get("lon") is None:
             resp.status = falcon.HTTP_405
-            resp.text = "Invalid input - some input fields may be missing"
+            resp.media = "Invalid input - some input fields may be missing"
             return
+        else:
+            # Generate missing details
+            new_item = {
+                'id': int(datetime.datetime.now().timestamp() * 1000000),  # 15-digit timestamp as an integer
+                **req_data,
+                'date_from': datetime.datetime.now().isoformat(), # Date in ISO format
+                'date_to': datetime.datetime.now().isoformat(),
+            }
 
-        # Generate missing details
-        new_item = {
-            'id': int(datetime.now().timestamp() * 1000000),  # 15-digit timestamp as an integer
-            **req_data,
-            'date_from': datetime.now().isoformat(), # Date in ISO format
-            'date_to': datetime.now().isoformat(),
-        }
+            # Add item to the list
+            items.append(new_item)
 
-        # Add item to the list
-        ItemsResource.items.append(new_item)
+            # Return 201 and the JSON data
+            resp.status = falcon.HTTP_201
+            resp.media = new_item
 
-        # Return 201 and the JSON data
-        resp.status = falcon.HTTP_201
-        resp.text = json.dumps(new_item, indent=2)
-
-class DeleteItemResource:
-    def on_delete(self, req, resp, item_id):
-        # Parse the ID parameter
+class ItemsResource:
+    def on_get(self, req, resp, item_id):
         item_id = int(item_id)
 
-        # Find the index of the item with the specified ID
-        item_index = items.get_item_index_by_id(item_id)
+        for i in items:
+            if i['id'] == item_id:
+                resp.media = i
+                resp.status = falcon.HTTP_200
+                break
+            else:
+                resp.media = {'error': 'Item not found'}
+                resp.status = falcon.HTTP_404
+    
+    def on_delete(self, req, resp, item_id):
+        # Parse the ID parameter
+        id = int(item_id)
+        itemIndex = None
+
+        for index, item in enumerate(items):
+            if item['id'] == id:
+                itemIndex = index
+                break
+
 
         # Check if the item was found
-        if item_index == -1:
+        if itemIndex == None:
             resp.status = falcon.HTTP_404 # Return 404 if no item was found
             resp.text = "Item not found"
-            return
+        else:
+            # Delete the item from the ITEMS array
+            items.pop(itemIndex)
 
-        # Delete the item from the ITEMS array
-        ItemsResource.items.pop(item_index)
+            # Respond with a 204 status code indicating a successful deletion
+            resp.status = falcon.HTTP_204
 
-        # Respond with a 204 status code indicating a successful deletion
-        resp.status = falcon.HTTP_204
+class GetItemsResource:
+    def on_options(self, req, resp):
+        # Respond to CORS requests
+        resp.set_header('Access-Control-Allow-Origin', '*')
 
-    def get_item_index_by_id(self, item_id):
-        for i, item in enumerate(ItemsResource.items):
-            if item["id"] == item_id:
-                return i
-        return -1
+    def on_get(self, req, resp): ## Handles GET /items
+        resp.media = items
+        resp.status = falcon.HTTP_200
 
 app = falcon.App(middleware=[cors.middleware])  # Instantiate App with CORS middleware
 
-# Create instances of the resource classes
-root = RootResource()
-items = ItemsResource()
-item = ItemResource()
-create_item = CreateItemResource()
-delete_item = DeleteItemResource()
-
 # ENDPOINTS
-app.add_route("/", root)
-app.add_route('/items', items)
-app.add_route("/item/{item_id}", item)
-app.add_route("/item", create_item, methods=['POST'])
-app.add_route("/item/{item_id}", delete_item, methods=['DELETE'])
+app.add_route("/", RootResource())
+app.add_route('/items', GetItemsResource())
+app.add_route("/item/{item_id}", ItemsResource())
+app.add_route("/item", CreateItemResource())
+#app.add_route("/item/{item_id}", delete_item, methods=['DELETE'])
 
 if __name__ == "__main__":
     with make_server("", 8000, app) as httpd:
